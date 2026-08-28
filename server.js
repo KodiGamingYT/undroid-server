fb92665fd0b23bdc25b7b331f6b8a596ac86b635
CodyTheLilDev <konrad.durau@gmail.com>
2026-08-28 15:53:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
-// Tworzymy serwer HTTP (wymagane przez Socket.IO)
 const server = http.createServer(app);
-// Inicjalizacja Socket.IO z obsługą CORS
 const io = new Server(server, {
     cors: { origin: "*" }
 });
@@ -15,19 +13,21 @@ const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 
-// Serwowanie pliku strony głównej
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Nasłuchiwanie szybkich połączeń Socket.IO
 io.on('connection', (socket) => {
     console.log('Nawiązano nowe połączenie:', socket.id);
 
-    // Kiedy przeglądarka wyśle ruch myszy/kliknięcie...
+    // Kiedy przyjdzie ruch myszką z PC
     socket.on('mouse_event', (data) => {
-        // ...natychmiast przekaż te dane do WSZYSTKICH innych podłączonych klientów (czyli do Pythona)
         socket.broadcast.emit('mouse_event', data);
+    });
+
+    // NOWE: Kiedy przyjdzie TAP/Dotknięcie palcem z Kindle'a
+    socket.on('tap_event', (data) => {
+        socket.broadcast.emit('tap_event', data);
     });
 
     socket.on('disconnect', () => {
@@ -35,7 +35,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// Używamy server.listen, a nie app.listen!
 server.listen(PORT, () => {
     console.log(`Un-Droid WebSockets Server działa na porcie ${PORT}`);
 });
