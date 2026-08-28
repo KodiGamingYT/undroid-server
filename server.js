78f8ed7043bdfb791371f762e5941ea54f3f6c24
CodyTheLilDev <konrad.durau@gmail.com>
2026-08-28 12:23:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,29 +3,16 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 10000;
 
-let mouseEvents = [];
-
 app.use(express.json());
 
-// Odbieranie sprzętowych akcji myszy z wirtualnego touchpada Pointer Lock
-app.get('/mouse_event', (req, res) => {
-    mouseEvents.push({
-        action: req.query.action,
-        dx: parseInt(req.query.dx || 0),
-        dy: parseInt(req.query.dy || 0),
-        button: parseInt(req.query.button || 0)
-    });
+let isVncActive = false;
+app.get('/vnc_active', (req, res) => {
+    isVncActive = true;
     res.send("OK");
-});
-
-// Oddawanie zgromadzonych ruchów do skryptu w Pythonie
-app.get('/get_clicks', (req, res) => {
-    res.json(mouseEvents);
-    mouseEvents = []; // Błyskawiczne czyszczenie kolejki, aby uniknąć lagów
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Hardware Mouse Server active on port ${PORT}`));
+app.listen(PORT, () => console.log(`VNC Web Portal active on port ${PORT}`));
